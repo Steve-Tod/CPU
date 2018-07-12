07f8ed98f2ab93c9c95917b4864742080a376ca7
Steve-Tod <785590489@qq.com>
2018-07-12 17:30:06 +0800

    add br_clk_16, modify .gitignore

---
+++ single_cycle/br_clk_16.v
@@ -0,0 +1,27 @@
+module br_clk_16(sysclk, brclk16);
+input   sysclk;
+output  reg brclk16;
+
+parameter   CNT_NUM = 10'd325;
+reg [9:0]   cnt;
+
+initial
+begin
+    brclk16 = 0;
+    cnt = 0;
+end
+
+always @(posedge sysclk)
+begin
+    if (cnt >= CNT_NUM)
+        cnt <= 10'd0;
+    else
+        cnt <= cnt + 10'd1;
+
+    if (cnt <= 10'd162)
+        brclk16 <= 1;
+    else
+        brclk16 <= 0;
+end
+
+endmodule
