53844df4458564f17f4df1ec286d93842408159a
Tod <785590489@qq.com>
2018-07-15 00:18:32 +0800

    modify file structure

--- ref codes and tb from 2012/各模块代码及testbench/CPU_1.v
+++
@@ -1,9 +0,0 @@
-module CPU_1(system_clk,reset,led,AN,digital,UART_RX,UART_TX);
-  input system_clk,reset,UART_RX;
-  output UART_TX;
-  output[7:0] led,digital;
-  output[3:0] AN;
-  wire baud_rate_clock;
-  CPU_baud_rate baud_rate(system_clk,reset,baud_rate_clock);
-  CPU_Pipeline pipeline(system_clk,reset,led,AN,digital,UART_TX,UART_RX,baud_rate_clock);  
-endmodule
