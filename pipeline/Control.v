ac1431ed29358e0fa106e87c31b450260a25bae4
Tod <785590489@qq.com>
2018-07-16 14:24:41 +0800

    add files to pipeline

---
+++ pipeline/Control.v
@@ -0,0 +1,190 @@
+module Control(PC,OpCode, Funct, IRQ ,
+	PCSrc, Sign, RegWrite, RegDst, 
+	MemRead, MemWrite, MemtoReg, 
+	ALUSrc1, ALUSrc2, ExtOp, LuOp, ALUFun);
+
+	input [5:0] OpCode;
+	input [5:0] Funct;
+    input IRQ;
+    input PC;//PC[31]
+
+	output [2:0] PCSrc;
+	output Sign;
+	output RegWrite;
+	output [1:0] RegDst;
+	output MemRead;
+	output MemWrite;
+	output [1:0] MemtoReg;
+	output ALUSrc1;
+	output ALUSrc2;
+	output ExtOp;
+	output LuOp;
+	output reg [5:0] ALUFun;
+
+    parameter ALUADD = 6'b00_0000;
+    parameter ALUSUB = 6'b00_0001;
+    parameter ALUAND = 6'b01_1000;
+    parameter ALUOR  = 6'b01_1110;
+    parameter ALUXOR = 6'b01_0110;
+    parameter ALUNOR = 6'b01_0001;
+    parameter ALUSLL = 6'b10_0000;
+    parameter ALUSRL = 6'b10_0001;
+    parameter ALUSRA = 6'b10_0011;
+    parameter ALUEQ =  6'b11_0011;
+    parameter ALUNEQ = 6'b11_0001;
+    parameter ALULT =  6'b11_0101;
+    parameter ALULEZ = 6'b11_1101;
+    parameter ALULTZ = 6'b11_1011;
+    parameter ALUGTZ = 6'b11_1111; 
+    //add a function
+    //i dont know how to use it
+    //but it's requested
+    parameter ALUA = 6'b01_1010; 
+
+
+    wire UnDefine;
+    assign UnDefine =
+    (    OpCode == 6'h01    ||
+         OpCode == 6'h02    ||
+         OpCode == 6'h03    ||
+         OpCode == 6'h04    ||
+         OpCode == 6'h05    ||
+         OpCode == 6'h06    ||
+         OpCode == 6'h07    ||
+         OpCode == 6'h08    ||
+         OpCode == 6'h09    ||
+         OpCode == 6'h0a    ||
+         OpCode == 6'h0b    ||
+         OpCode == 6'h0c    ||
+         OpCode == 6'h0f    ||
+         OpCode == 6'h23    ||
+         OpCode == 6'h2b) ? 1'b0 : 
+    (   OpCode == 6'h00 &&
+       (Funct == 6'h20 ||
+        Funct == 6'h21 ||
+        Funct == 6'h22 ||
+        Funct == 6'h23 ||
+        Funct == 6'h24 ||
+        Funct == 6'h25 ||
+        Funct == 6'h26 ||
+        Funct == 6'h27 ||
+        Funct == 6'h2a ||
+        Funct == 6'h00 ||
+        Funct == 6'h02 ||
+        Funct == 6'h03 ||
+        Funct == 6'h08 ||
+        Funct == 6'h09) 
+    )? 1'b0 : 1'b1; 
+
+
+	assign XADR = ~PC & UnDefine;
+    assign ILLOP = ~PC & IRQ;
+
+	assign PCSrc[2:0] =
+         ILLOP? 3'b100 :
+         XADR? 3'b101 :
+	(    OpCode == 6'h04 || 
+         OpCode == 6'h05 || 
+         OpCode == 6'h06 || 
+         OpCode == 6'h07 || 
+         OpCode == 6'h01) ? 3'b001:
+	(    OpCode == 6'h02 || 
+         OpCode == 6'h03) ? 3'b010:
+	(    OpCode == 6'h00 && 
+        ( Funct == 6'h08 || Funct == 6'h09)) ? 3'b011 : 3'b000;
+	assign Sign = 
+    //exist question
+	(   (OpCode == 6'h00 && 
+        (Funct == 6'h20 || Funct == 6'h22)) || 
+        OpCode == 6'h08 || 
+        OpCode == 6'h0a || 
+        OpCode == 6'h01 || 
+        OpCode == 6'h04 || 
+        OpCode == 6'h05 ||
+        OpCode == 6'h06 || 
+        OpCode == 6'h07) ? 1:
+		 0;
+	assign RegWrite = 
+        (ILLOP || XADR)? 1 :   //We should keep the PC + 4 in this situation
+	(   OpCode == 6'h2b || 
+        OpCode == 6'h04 ||
+        OpCode == 6'h02 || 
+        OpCode == 6'h05 || 
+        OpCode == 6'h06 || 
+        OpCode == 6'h07 ||  
+        OpCode == 6'h01 ||
+        (OpCode == 6'h00 && Funct == 6'h08)) ? 0:
+		1;
+	assign RegDst[1:0] = 
+        (ILLOP || XADR)? 2'b11:
+		(OpCode == 6'h00)? 2'b00:
+		(OpCode == 6'h03)? 2'b10:
+		2'b01;
+	assign MemRead = 
+		(OpCode == 6'h23)? 1:
+		0;
+	assign MemWrite = 
+		(OpCode == 6'h2b)?1:
+		0;
+	assign MemtoReg[1:0]=
+        ILLOP ? 2'b11 :            //choose PC
+        XADR ? 2'b10:                  //choose PC + 4
+	(    OpCode == 6'h03 || 
+        (OpCode == 6'h00 && Funct == 6'h09))? 2'b10:
+    	(OpCode == 6'h23)? 2'b01:
+        2'b00;
+	assign ALUSrc1 = 
+	(   OpCode == 6'h00 && 
+        (Funct == 6'h00 || 
+        Funct == 6'h02  || 
+        Funct == 6'h03))? 1:
+		0;
+	assign ALUSrc2 =
+	(   OpCode == 6'h00 || 
+        OpCode == 6'h04 || 
+        OpCode == 6'h05 || 
+        OpCode == 6'h06 ||
+        OpCode == 6'h07 || 
+        OpCode == 6'h01 )? 0:
+		1;
+	assign ExtOp = 
+    //exist question
+		(OpCode == 6'h0c)? 0:
+		1;
+	assign LuOp = 
+    //exist question
+		(OpCode == 6'h0f)? 1:
+		0;
+	
+    reg [5:0]ALUOp;
+	always @(*)
+        case (Funct)
+            6'h00: ALUOp <= ALUSLL;
+            6'h02: ALUOp <= ALUSRL;
+            6'h03: ALUOp <= ALUSRA;
+            6'h20: ALUOp <= ALUADD;
+            6'h21: ALUOp <= ALUADD;
+            6'h22: ALUOp <= ALUSUB;
+            6'h23: ALUOp <= ALUSUB;
+            6'h24: ALUOp <= ALUAND;
+            6'h25: ALUOp <= ALUOR;
+            6'h26: ALUOp <= ALUXOR;
+            6'h27: ALUOp <= ALUNOR;
+            6'h2a: ALUOp <= ALULT;
+            default: ALUOp <= ALUADD;
+        endcase
+    always @(*)
+        case (OpCode)
+          6'h00: ALUFun <= ALUOp;
+          6'h0c: ALUFun <= ALUAND;
+          6'h04: ALUFun <= ALUEQ;
+          6'h05: ALUFun <= ALUNEQ;
+          6'h0a: ALUFun <= ALULT;
+          6'h0b: ALUFun <= ALULT;
+          6'h06: ALUFun <= ALULEZ;
+          6'h01: ALUFun <= ALULTZ;
+          6'h07: ALUFun <= ALUGTZ;
+          default: ALUFun <= ALUADD;
+        endcase    	
+	
+endmodule
---
+++ pipeline/Peripheral.v
@@ -0,0 +1,139 @@
+`timescale 1ns/1ps
+
+module Peripheral (reset,clk,rd,wr,addr,wdata,rdata,led,switch,digi,irqout,
+                   UART_RX, UART_TX, PC31);
+input reset,clk,PC31;
+input rd,wr;
+input [31:0] addr;
+input [31:0] wdata;
+output [31:0] rdata;
+reg [31:0] rdata;
+
+output [7:0] led;
+reg [7:0] led;
+input [7:0] switch;
+output [11:0] digi;
+reg [11:0] digi;
+output irqout;
+
+// UART
+input UART_RX;
+output UART_TX;
+
+wire    brclk16;
+reg     [7:0]   UART_TXD;
+reg     [7:0]   UART_RXD;
+wire    TX_STATUS;
+wire    RX_STATUS;
+wire    [7:0]   TX_DATA;
+wire    [7:0]   RX_DATA;
+reg     TX_EN;
+reg     [4:0]   UART_CON;
+reg     reading, writing;
+reg     [9:0]   TX_cnt;
+parameter   CNT_NUM = 10'd325;
+
+br_clk_16 br_clk1(clk, brclk16);
+receiver receiver1(reset, UART_RX, brclk16, RX_DATA, RX_STATUS);
+sender sender1(reset, brclk16, TX_DATA, TX_EN, TX_STATUS, UART_TX);
+
+reg [31:0] TH,TL;
+reg [2:0] TCON;
+
+initial begin
+	TX_EN = 0;
+	TX_cnt = 0;
+	TCON = 3'b0;
+	UART_CON[4] = 0;
+end
+
+assign irqout = (~PC31) & TCON[2];
+
+always@(*) begin
+    if(rd) begin
+        case(addr)
+            32'h40000000: rdata <= TH;          
+            32'h40000004: rdata <= TL;          
+            32'h40000008: rdata <= {29'b0,TCON};                
+            32'h4000000C: rdata <= {24'b0,led};         
+            32'h40000010: rdata <= {24'b0,switch};
+            32'h40000014: rdata <= {20'b0,digi};
+            32'h40000018 : rdata <= {24'b0, UART_TXD};
+            32'h4000001c : rdata <= {24'b0, UART_RXD};
+            32'h40000020 : rdata <= {27'b0, UART_CON};
+            default: rdata <= 32'b0;
+        endcase
+    end
+    else
+        rdata <= 32'b0;
+end
+
+always@(negedge reset or posedge clk) begin
+    if(~reset) begin
+        TH <= 32'b0;
+        TL <= 32'b0;
+        TCON <= 3'b0; 
+        UART_CON <= 0;
+        TX_EN <= 0;
+        reading <= 0;
+        TX_cnt <= 0;
+		UART_CON[1:0] <= 2'b11;
+    end
+    else begin
+		UART_CON[4] <= ~TX_STATUS;
+        if(TCON[0]) begin   //timer is enabled
+            if(TL==32'hffffffff) begin
+                TL <= TH;
+                if(TCON[1]) TCON[2] <= 1'b1;        //irq is enabled
+            end
+            else TL <= TL + 1;
+        end
+        //read
+        //set 0
+        if (rd && (addr == 32'h4000_0020)) begin
+            UART_CON[3:2] <= 2'b0;
+        end
+        //use reading to avoid setting repeatedly
+        if (~RX_STATUS) reading <= 0;
+        if (RX_STATUS && ~reading && UART_CON[1]) begin
+            UART_CON[3] <= 1;
+            UART_RXD <= RX_DATA;
+            reading <= 1;
+        end
+
+        // lengthen the TX_EN pulse
+        if (TX_EN) begin
+            if (TX_cnt < CNT_NUM) TX_cnt <= TX_DATA + 1;
+            else begin
+                TX_cnt <= 0;
+                TX_EN <= 0;
+            end
+        end
+        
+        if(wr) begin
+            case(addr)
+                32'h40000000: TH <= wdata;
+                32'h40000004: TL <= wdata;
+                32'h40000008: TCON <= wdata[2:0];       
+                32'h4000000C: led <= wdata[7:0];            
+                32'h40000014: digi <= wdata[11:0];
+                32'h40000018: begin
+                    //since sender has cache, we don't need to worry if new TXD will affect the sending one
+                    UART_TXD <= wdata[7:0];
+                    if (TX_STATUS && ~TX_EN && UART_CON[0]) begin
+                        TX_EN <= 1;
+                    end
+				end
+				//UART_RXD should not be written
+				32'h4000001c : UART_RXD <= wdata [7:0];
+				32'h40000020 : UART_CON <= wdata [4:0];
+                default: ;
+            endcase
+        end
+    end
+end
+endmodule
+
+
+
+
